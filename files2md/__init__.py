2f287c95397e1115e76d569144a82a32ee30f3d1
KacperKotlewski <kotlewski.kacper@gmail.com>
2020-02-19 22:01:25 +0100

    another bugfix vol.2

--- files2md/__init__.py
+++ files2md/__init__.py
@@ -1,4 +1,18 @@
-from .files2markdown import tree2md
+import files2md.config
+import files2md.structure_objects
+import files2md.to_markdown
+
+def tree2md():
+    from files2md.structure_objects import DirectoryObj
+    from files2md.to_markdown import ToMarkdownFile
+    import os
+    currentDir = os.getcwd()
+    currentDirName = str(os.path.basename(currentDir))
+
+    dir = DirectoryObj(name=currentDirName, dir=currentDir)
+    dir.autoAddFile(autoAddSubdirs=True)
+    ToMarkdownFile(dir.getTree()).save()
+
 
 if __name__ == "__main__":
     tree2md()
--- files2md/to_markdown/__init__.py
+++ files2md/to_markdown/__init__.py
@@ -1,1 +1,2 @@
 from .to_markdown_file import ToMarkdownFile
+from .files_to_markdown import tree2md
